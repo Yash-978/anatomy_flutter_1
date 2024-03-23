27e54acae27d115204be6af8fd6a1d36b63324c1
Yash-978 <virwalyash@gmail.com>
2024-03-23 21:16:32 +0530

    15-3-2024

--- lib/Lec 4.dart
+++ lib/Lec 4.dart
@@ -6,10 +6,54 @@ void main() {
       debugShowCheckedModeBanner: false,
       title: '',
       home: Scaffold(
-
-      appBar: ,,
-
-      )
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+          title: const Text(
+            'My Rnw',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400),
+          ),
+        ),
+        body: const Center(
+          child: Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                    text: 'Red & White\n',
+                    style: TextStyle(
+                      letterSpacing: 3,
+                      color: Colors.red,
+                      height: 1,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 55,
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.red,
+                      decorationThickness: 1.5,
+                    )),
+                TextSpan(
+                  text: '\t\t\t\t\t\tMultimedia Education\n',
+                  style: TextStyle(
+                    // letterSpacing: 0.01,
+                    color: Colors.red,
+                    height: 1,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
+                TextSpan(
+                  text: '\t\t\t\t\tShaping "skills" for "scaling" higher...!!!',
+                  style: TextStyle(
+                    letterSpacing: 1,
+                    color: Colors.red,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     ),
   );
 }
